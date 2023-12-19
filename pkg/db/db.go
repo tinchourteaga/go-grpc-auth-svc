df4f47a747b20025ac03b8b16975176fa387eb73
tinchourteaga <38136900+tinchourteaga@users.noreply.github.com>
2023-12-19 14:37:51 -0300

    db connector

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,1 +1,24 @@
 package db
+
+import (
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+	"github.com/tinchourteaga/go-grpc-auth-svc/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Connector struct {
+	DB *gorm.DB
+}
+
+func NewDatabaseConnection() Connector {
+	db, err := gorm.Open(postgres.Open(viper.GetString("DB_URL")), &gorm.Config{})
+	if err != nil {
+		log.Fatal().Msg("cannot connect to database")
+	}
+
+	db.AutoMigrate(&models.User{})
+
+	return Connector{DB: db}
+}
