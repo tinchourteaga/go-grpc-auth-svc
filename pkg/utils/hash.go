b7d055ccbffb5ca27762878712879485bed7bec4
tinchourteaga <38136900+tinchourteaga@users.noreply.github.com>
2023-12-19 14:37:41 -0300

    hashing and jwt utils

--- pkg/utils/hash.go
+++ pkg/utils/hash.go
@@ -1,1 +1,22 @@
 package utils
+
+import (
+	"github.com/rs/zerolog/log"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		log.Error().Msg("error hashing password")
+		return ""
+	}
+
+	return string(bytes)
+}
+
+func PasswordMatchesHash(password string, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+
+	return err == nil
+}
--- pkg/utils/jwt.go
+++ pkg/utils/jwt.go
@@ -1,1 +1,69 @@
 package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/rs/zerolog/log"
+	"github.com/tinchourteaga/go-grpc-auth-svc/internal/models"
+)
+
+type JwtWrapper struct {
+	SecretKey       string
+	Issuer          string
+	ExpirationHours int64
+}
+
+type jwtClaims struct {
+	Id    int64
+	Email string
+	jwt.StandardClaims
+}
+
+func (w *JwtWrapper) GenerateToken(user models.User) (signedToken string, err error) {
+	claims := &jwtClaims{
+		Id:    user.Id,
+		Email: user.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(w.ExpirationHours)).Unix(),
+			Issuer:    w.Issuer,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err = token.SignedString([]byte(w.SecretKey))
+	if err != nil {
+		log.Error().Msg("error retrieving signed token")
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func (w *JwtWrapper) ValidateToken(signedToken string) (claims *jwtClaims, err error) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&jwtClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(w.SecretKey), nil
+		},
+	)
+
+	if err != nil {
+		return nil, errors.New("couldn't parse claims")
+	}
+
+	claims, ok := token.Claims.(*jwtClaims)
+
+	if !ok {
+		return nil, errors.New("couldn't parse claims")
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		return nil, errors.New("jwt is expired")
+	}
+
+	return claims, nil
+}
